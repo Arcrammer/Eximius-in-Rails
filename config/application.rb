4b2fbc5714262a877c57cca39b6af0ae5bb3da04
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-11-25 13:35:49 -0500

    Finish Three Panels; Finish 'Ready to Begin'

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Eximius
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # More assets that should be precompiled
+    config.assets.paths << Rails.root.join('app', 'assets', 'icons')
   end
 end
